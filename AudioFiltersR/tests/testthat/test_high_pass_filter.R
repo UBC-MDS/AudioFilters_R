5eb37ea738f5d5c0907019af411127eb3545f5bc
Marcelle Chiriboga Carvalho <42902889+mchiriboga@users.noreply.github.com>
2019-02-09 14:20:22 -0800

    Update values from 440Hz to 1000Hz to match expected output file

--- AudioFiltersR/tests/testthat/test_high_pass_filter.R
+++ AudioFiltersR/tests/testthat/test_high_pass_filter.R
@@ -2,9 +2,9 @@
 
 input_signal <- read.csv("data/bark.csv", colClasses=c('numeric'))[[1]]
 
-test_that("Result of filtering a known input signal with cutoff frequency of 440hz matches the expected output", {
+test_that("Result of filtering a known input signal with cutoff frequency of 1000Hz matches the expected output", {
   expected_output <- read.csv("data/hipass/bark_highpass_1000Hz_24db.csv", colClasses=c('numeric'))[[1]]
-  output_signal <- high_pass_filter(input_signal, 440)
+  output_signal <- high_pass_filter(input_signal, 1000)
 
   expect_equal(output_signal, expected_output)
 })
