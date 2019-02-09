f731808e66b46b56a1cc48020323a9f4b93c689e
Marcelle Chiriboga <marcelle.chiriboga@gmail.com>
2019-02-08 21:52:48 -0800

    Add test functions

--- AudioFiltersR/tests/testthat/test_high_pass_filter.R
+++ AudioFiltersR/tests/testthat/test_high_pass_filter.R
@@ -1,9 +1,10 @@
-#' High Pass Filter
-#'
-#' @return
-#' @export
-#'
-#' @examples
-high_pass_filter <- function() {
-  print("High pass filter!")
-}
+# This script tests the change_speed() function
+
+input_signal <- read.csv("data/bark.csv", colClasses=c('numeric'))[[1]]
+
+test_that("Result of filtering a known input signal with cutoff frequency of 440hz matches the expected output", {
+  expected_output <- read.csv("data/change_speed/bark_440.csv", colClasses=c('numeric'))[[1]]
+  output_signal <- high_pass_filter(input_signal, 440)
+
+  expect_equal(output_signal, expected_output)
+})
