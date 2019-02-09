d8b192cb5e48acd4bec41ececc38f47109483631
Paul Vial <vial.paul@gmail.com>
2019-02-09 14:11:56 -0800

    - updated hipass filter specifications to say that attenuation will be by 24 db (otherwise w would need to add an additioinal parameter argument) - added highpass filter test file and updated hipass filter test script to point to the file - added the original "bark" wavefile to the repo

--- AudioFiltersR/tests/testthat/test_high_pass_filter.R
+++ AudioFiltersR/tests/testthat/test_high_pass_filter.R
@@ -1,9 +1,9 @@
-# This script tests the change_speed() function
+# This script tests the high_pass_filter function
 
 input_signal <- read.csv("data/bark.csv", colClasses=c('numeric'))[[1]]
 
 test_that("Result of filtering a known input signal with cutoff frequency of 440hz matches the expected output", {
-  expected_output <- read.csv("data/change_speed/bark_440.csv", colClasses=c('numeric'))[[1]]
+  expected_output <- read.csv("data/hipass/bark_highpass_1000Hz_24db.csv", colClasses=c('numeric'))[[1]]
   output_signal <- high_pass_filter(input_signal, 440)
 
   expect_equal(output_signal, expected_output)
