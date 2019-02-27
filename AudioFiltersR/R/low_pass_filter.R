60f2aaab1a5a2db0dfa4715fa0e2dc94f12b96fd
Paul Vial <vial.paul@gmail.com>
2019-02-26 16:03:04 -0800

    replaced test file and changed "high" to "low"

--- AudioFiltersR/R/low_pass_filter.R
+++ AudioFiltersR/R/low_pass_filter.R
@@ -1,11 +1,11 @@
 #' Filter audio signal, attenuating frequencies below a cut off.
 #'
 #' @param input_signal numeric
-#' @param cutoff_frequency numeric, cutoff frequency of the high pass filter.
+#' @param cutoff_frequency numeric, cutoff frequency of the low pass filter.
 #'
 #' @return numeric, vector representing the audio signal with frequencies below the cut off attenuated by 24 db.
 #' @export
 #'
-high_pass_filter <- function(input_signal, cutoff_frequency) {
-  print("High pass filter!")
+low_pass_filter <- function(input_signal, cutoff_frequency) {
+  print("Low pass filter!")
 }
--- AudioFiltersR/tests/testthat/test_low_pass_filter.R
+++ AudioFiltersR/tests/testthat/test_low_pass_filter.R
@@ -1,10 +1,10 @@
-# This script tests the high_pass_filter function
+# This script tests the low_pass_filter function
 
 input_signal <- read.csv("data/bark.csv", colClasses=c('numeric'))[[1]]
 
 test_that("Result of filtering a known input signal with cutoff frequency of 1000Hz matches the expected output", {
-  expected_output <- read.csv("data/hipass/bark_highpass_1000Hz_24db.csv", colClasses=c('numeric'))[[1]]
-  output_signal <- high_pass_filter(input_signal, 1000)
+  expected_output <- read.csv("data/lowpass/bark_lowpass_1000Hz.csv", colClasses=c('numeric'))[[1]]
+  output_signal <- low_pass_filter(input_signal, 1000)
 
   expect_equal(output_signal, expected_output)
 })
