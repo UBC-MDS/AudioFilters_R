1e4de101711f7d1e25d500861affcfbdd09eadf4
Paul Vial <vial.paul@gmail.com>
2019-02-26 21:46:22 -0800

    finished low-pass function and its tests

--- AudioFiltersR/R/low_pass_filter.R
+++ AudioFiltersR/R/low_pass_filter.R
@@ -3,11 +3,39 @@
 #' @param input_signal numeric
 #' @param cutoff_frequency numeric, cutoff frequency of the low pass filter.
 #'
-#' @return numeric, vector representing the audio signal with frequencies below the cut off attenuated by 24 db.
+#' @return numeric, vector representing the audio signal with frequencies below the cut off attenuated.
 #' @export
 #'
+#' @examples
+#' low_pass_filter(input_signal, 1000)
+
 low_pass_filter <- function(input_signal, cutoff_frequency) {
-  bf <- signal::butter(3, 1/cutoff_frequency)       # low-pass filter
-  output_signal <- signal::filter(bf, input_signal) # apply filter
+
+  # Raise error if cutoff_frequency is not positive
+  if(cutoff_frequency <= 0){
+    stop("Error: cutoff frequency must be a positive number")
+  }
+
+  # Raise error if input_signal is of an unsupported type
+  if(class(input_signal) != "numeric"){
+    stop("Error: input_signal must be numeric")
+  }
+
+  # Create filter
+  lp_filter <- signal::butter(3, 1/cutoff_frequency)       # low-pass filter
+
+  # Apply the filter to the input signal
+  output_signal <- signal::filter(lp_filter, input_signal) # apply filter
   return(output_signal)
 }
+
+#TBD input signal not null
+
+
+# # Raise error if input_signal is of an unsupported type
+# if input_signal.dtype.kind not in 'iu' and input_signal.dtype.kind != 'f' :
+#   raise TypeError("'input_signal' must be an array of integers or floats")
+#
+# # Raise error if cutoff_frequency is not positive
+# if cutoff_frequency <= 0:
+#   raise Exception('cutoff frequency must be a positive number')
--- AudioFiltersR/tests/testthat/test_low_pass_filter.R
+++ AudioFiltersR/tests/testthat/test_low_pass_filter.R
@@ -5,6 +5,8 @@ input_signal <- read.csv("data/bark.csv", colClasses=c('numeric'))[[1]]
 test_that("Result of filtering a known input signal with cutoff frequency of 1000Hz matches the expected output", {
   expected_output <- read.csv("data/lowpass/bark_lowpass_1000Hz.csv", colClasses=c('numeric'))[[1]]
   output_signal <- low_pass_filter(input_signal, 1000)
+
+  # Mean squared error between input and output signal
   mse <- mean(
     ((expected_output - output_signal)**2)
   )
@@ -12,14 +14,10 @@ test_that("Result of filtering a known input signal with cutoff frequency of 100
   expect_lt(mse, 0.002)
 })
 
+test_that("Eexception is raised for unsupported input_signal argument type", {
+  expect_error(low_pass_filter(input_signal, 0), "Error: cutoff frequency must be a positive number")
+})
 
-#Python version:
-# def test_low_pass_filter_attenuates_frequencies_below_1000_cutoff():
-#   expected_output = np.genfromtxt('tests/data/lowpass/bark_lowpass_1000Hz.csv', dtype = 'float32')
-# output_signal = low_pass_filter(input_signal, 1000)
-#
-# # Mean squared error between input and output signal
-# mse = ((expected_output - output_signal)**2).mean(axis=0)
-# print(mse)
-#
-# assert mse < 0.00001, "Output does not match test data!"
+test_that("Exception is raised for invalid zero cutoff_frequency argument", {
+  expect_error(low_pass_filter("test", 500), "Error: input_signal must be numeric")
+})
