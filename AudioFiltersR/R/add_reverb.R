f79b68f498b4e5fd7e34f5f3e8965210bbbc8d93
Marcelle Chiriboga <marcelle.chiriboga@gmail.com>
2019-03-01 00:18:36 -0800

    Implement add_reverb function

--- AudioFiltersR/R/add_reverb.R
+++ AudioFiltersR/R/add_reverb.R
@@ -7,5 +7,19 @@
 #' @export
 #'
 add_reverb <- function(input_signal, type = 'hall') {
-  print("Add reverb")
+
+  if (type == 'hall') {
+    impulse_response <- read.csv(system.file("impulse_responses", "impulse_hall.csv", package = "AudioFiltersR"), colClasses=c('numeric'))[[1]]
+  } else if (type == 'church') {
+    impulse_response <- read.csv(system.file("impulse_responses", "impulse_church.csv", package = "AudioFiltersR"), colClasses=c('numeric'))[[1]]
+  } else {
+    stop("Error: Unsupported reverb type. Type must be 'hall' or 'church'")
+  }
+
+  convolved_signal <- convolve(input_signal, rev(impulse_response), type = 'open')
+  output_signal <- tuneR::normalize(
+    tuneR::Wave(left= convolved_signal, bit = 32, pcm = FALSE, samp.rate = 44100), unit = '32'
+  )
+
+  return(output_signal@left[-1])
 }
