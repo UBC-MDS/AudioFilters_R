18646888f89a6c30225f837260d6e9f88889239c
Socorro <sedv8808@gmail.com>
2019-03-01 21:10:06 -0800

    Update change speed tests and expected outputs

--- AudioFiltersR/inst/tests/testthat/test_change_speed.R
+++ AudioFiltersR/inst/tests/testthat/test_change_speed.R
@@ -3,14 +3,14 @@
 input_signal <- read.csv("data/bark.csv", colClasses=c('numeric'))[[1]]
 
 test_that("Result of changing the speed of a known input signal with a rate of 2 matches the expected output", {
-  expected_output <- read.csv("data/change_speed/bark_double_speed.csv", colClasses=c('numeric'))[[1]]
+  expected_output <- read.table("data/change_speed/bark_double_speed.csv", colClasses=c('numeric'))[[1]]
   output_signal <- change_speed(input_signal, 2.0)
 
   expect_equal(output_signal, expected_output)
 })
 
 test_that("Result of changing the speed of a known input signal with a rate of 0.5 matches the expected output", {
-  expected_output <- read.csv("data/change_speed/bark_half_speed.csv", colClasses=c('numeric'))[[1]]
+  expected_output <- read.table("data/change_speed/bark_half_speed.csv", colClasses=c('numeric'))[[1]]
   output_signal <- change_speed(input_signal, 0.5)
 
   expect_equal(output_signal, expected_output)
