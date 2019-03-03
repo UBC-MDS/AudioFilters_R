a75012b42a939fe9e1a8e9d12bf9fe45d5b442d0
Marcelle Chiriboga <marcelle.chiriboga@gmail.com>
2019-03-02 19:05:21 -0800

    Update onLoad hook to install and load Python dependencies

--- AudioFiltersR/R/zzz.R
+++ AudioFiltersR/R/zzz.R
@@ -2,8 +2,16 @@ librosa <- NULL # nocov start
 np <- NULL
 
 .onLoad <- function(libname, pkgname) {
-  reticulate::py_install("librosa")
-  reticulate::py_install("numpy")
-  librosa <<- reticulate::import("librosa")
-  np <<- reticulate::import("numpy")
+  if (!reticulate::py_module_available("numpy")) {
+    reticulate::conda_install("r-reticulate", "numpy")
+  }
+
+  if (!reticulate::py_module_available("librosa")) {
+    reticulate::conda_install("r-reticulate", "librosa")
+  }
+
+  reticulate::use_condaenv("r-reticulate")
+
+  np <<- reticulate::import("numpy", delay_load = TRUE)
+  librosa <<- reticulate::import("librosa", delay_load = TRUE)
 } # nocov end
