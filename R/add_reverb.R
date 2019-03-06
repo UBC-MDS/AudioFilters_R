33a78e8f45f9445a39b96f1b80470c5afeb7d287
Paul Vial <vial.paul@gmail.com>
2019-03-06 00:10:09 -0800

    added stats and utils to DESCRIPTION file and function calls in reverb script

--- R/add_reverb.R
+++ R/add_reverb.R
@@ -14,14 +14,14 @@ add_reverb <- function(input_signal, type = 'hall') {
   }
 
   if (type == 'hall') {
-    impulse_response <- read.csv(system.file("impulse_responses", "impulse_hall.csv", package = "AudioFiltersR"), colClasses=c('numeric'))[[1]]
+    impulse_response <- utils::read.csv(system.file("impulse_responses", "impulse_hall.csv", package = "AudioFiltersR"), colClasses=c('numeric'))[[1]]
   } else if (type == 'church') {
-    impulse_response <- read.csv(system.file("impulse_responses", "impulse_church.csv", package = "AudioFiltersR"), colClasses=c('numeric'))[[1]]
+    impulse_response <- utils::read.csv(system.file("impulse_responses", "impulse_church.csv", package = "AudioFiltersR"), colClasses=c('numeric'))[[1]]
   } else {
     stop("Error: Unsupported reverb type. Type must be 'hall' or 'church'")
   }
 
-  convolved_signal <- convolve(input_signal, rev(impulse_response), type = 'open')
+  convolved_signal <- stats::convolve(input_signal, rev(impulse_response), type = 'open')
   output_signal <- tuneR::normalize(
     tuneR::Wave(left = convolved_signal, bit = 32, pcm = FALSE, samp.rate = 44100), unit = '32'
   )
